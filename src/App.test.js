a2258214b5e65c2865ec7ab981d2dc76a24cf17b
09arnold <09arnold.aq@gmail.com>
2020-04-05 20:20:40 +0000

    - 'Fixed' failing test - package-lock.json updates

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App page', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/VolaChat/i);
   expect(linkElement).toBeInTheDocument();
 });
